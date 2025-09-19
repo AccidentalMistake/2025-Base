1d92a79a768d57e5ba5fdc3124a8aed1e4847633
Accidental MIstake <18001870@students.sd308.org>
2025-09-18 20:13:55 -0500

    Changed Limitswitch ID

--- src/main/java/team/gif/robot/subsystems/LimitSwitch.java
+++ src/main/java/team/gif/robot/subsystems/LimitSwitch.java
@@ -1,13 +1,15 @@
 package team.gif.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import team.gif.robot.Robot;
+import team.gif.robot.RobotMap;
 
 public class LimitSwitch{
 
     private static DigitalInput LimitSwitch;
 
     public void limitSwitch() {
-        LimitSwitch = new DigitalInput(9);
+        LimitSwitch = new DigitalInput(RobotMap.LIMIT_SWITCH_ID);
     }
 
     public boolean getSwitchState(){

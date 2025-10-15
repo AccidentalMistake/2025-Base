ca68666b1a15fc57847f4fbebba3745b103beafa
Accidental MIstake <18001870@students.sd308.org>
2025-10-14 19:04:08 -0500

    Added Pneumatic subsystem and commands

--- src/main/java/team/gif/robot/commands/TalonJoystick.java
+++ src/main/java/team/gif/robot/commands/TalonJoystick.java
@@ -12,9 +12,7 @@ public class TalonJoystick extends Command {
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {
-
-    }
+    public void initialize() {}
 
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
---
+++ src/main/java/team/gif/robot/subsystems/PneumaticPiston.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team.gif.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.RobotMap;
+
+public class PneumaticPiston extends SubsystemBase {
+
+    private DoubleSolenoid PneumaticPiston;
+
+    public PneumaticPiston() {
+        PneumaticPiston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, RobotMap.PNEUMATIC_PISTON_IN_ID, RobotMap.PNEUMATIC_PISTON_OUT_ID);
+    }
+
+    public void Toggle(){PneumaticPiston.toggle();}
+
+}
